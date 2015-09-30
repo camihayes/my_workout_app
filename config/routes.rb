9dc4569ecb97cd13127eb6526022a23ef1cdcd83
camihayes <camihayes72@gmail.com>
2015-09-30 00:35:01 +0000

    create exercises successfully

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   get 'dashboard/index'
 
+  resources :users do
+    resources :exercises
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root to: 'dashboard#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
